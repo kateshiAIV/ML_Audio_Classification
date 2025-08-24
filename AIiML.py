f741d9241396437e70397417f352e7d2ed88c835
kateshiAIV <148715019+kateshiAIV@users.noreply.github.com>
2025-08-24 17:47:15 +0200

    test

--- AIiML.py
+++ AIiML.py
@@ -1,1 +1,15 @@
-print("Hello, World!")
+import os
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2" # Ukrycie ostrzeżeń CUDA i innych INFO/WARNING
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1" # Wyłączenie GPU, działanie tylko na CPU
+import cv2
+import matplotlib.pyplot as plt
+from transformers import ViTImageProcessor
+import numpy as np
+image = cv2.imread("test.jpg")
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+plt.imshow(image)
+plt.axis('off')
+plt.title("Oryginalny obraz")
+plt.show()
+processor = ViTImageProcessor.from_pretrained("google/vit-base-patch16-224")
+inputs = processor(images=image, return_tensors="tf")
